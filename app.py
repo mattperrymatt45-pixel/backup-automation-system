2718ac9c8e82f1d73d05371fb5bc7dd2f0d8ecdc
mattperrymatt45-pixel <mattperrymatt45@gmail.com>
2026-05-01 11:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,4 @@ def register_schedule(job_id, sched):
         t.start()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
